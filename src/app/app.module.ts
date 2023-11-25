598c3585f39f882568396779670e0d812fc20911
Andres Cadena <afcadenac@correo.udistrital.edu.co>
2023-11-24 22:13:54 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
+import { SharedModule } from './shared/shared.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GifsModule } from './gifs/gifs.module';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GifsModule,
+    SharedModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
